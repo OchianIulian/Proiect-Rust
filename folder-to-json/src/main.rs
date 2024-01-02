6d06a9096d2a9878f31044693201bec5f117c4fc
andrei <ochian_a@yahoo.ro>
2024-01-02 21:06:51 +0200

    feat: Take the folder path as argument

--- folder-to-json/src/main.rs
+++ folder-to-json/src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    /*take the folder path as argument from cmd */
+
+    //Retrieve command line arguments
+    let args: Vec<String> = env::args().collect();
+
+    //Check if at least one argument is passed (the first argument is program's name)
+    if args.len()<2 {
+        eprintln!("Trebuie sa introduci path ul");
+        return;
+    }
+
+    //Access the second argument (index 1) as folder path
+    let folder_path = &args[1];
+    println!("Folder path: {}", folder_path);
 }
---
+++ folder-to-json/target/debug/deps/folder_to_json-02d460db35b66f93.d
@@ -0,0 +1,5 @@
+/Volumes/D/facultate/An2/Semestrul 3/Rust/facultate/proiect/Proiect-Rust/folder-to-json/target/debug/deps/folder_to_json-02d460db35b66f93.rmeta: src/main.rs
+
+/Volumes/D/facultate/An2/Semestrul 3/Rust/facultate/proiect/Proiect-Rust/folder-to-json/target/debug/deps/folder_to_json-02d460db35b66f93.d: src/main.rs
+
+src/main.rs:
